89bbad3dc1c5581b9a77603023d77eb8462108b6
Dobrohotova <irina.dobrohotova@gmail.com>
2017-05-29 23:23:17 +0200

    contact page done

---
+++ contact.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Irina Dobrohotova">
+	<meta name="description" content="Irina Dobrohotova">
+	<meta name="keywords" content="Medhyatrops">
+	<meta name="viewport" content="">
+	<link rel="stylesheet" type="text/css" href="styles/index.css">
+	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,600,700|Raleway:300,400,600,700" rel="stylesheet">
+	<title>Medhyatrops</title>
+  <link href="styles/contact.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+	<?php include('header.html'); ?>
+	<div class="contact">
+		<h1>Contact</h1>
+		<div class="line1"></div>
+		<div class="line2"></div>
+		<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, ante quis congue ultrices, tortor tellus dictum elit, eu lobortis massa elit nec enim.</h3>
+	</div>
+	<div class="content_holder">
+		<div class="contact_form">
+			<form action='form.php' method="post">
+				<p>Your Name*</p>
+				<input type="text" name="name">
+				<p>Your Email*</p>
+				<input type="email" name="email">
+				<p>Phone*</p>
+				<input type="number" name="phone">
+				<p>Message*</p>
+				<textarea></textarea>
+				<button class="btn-2" type="submit">SUBMIT</button>
+	</form>
+		</div>
+		<div class="right_info">
+			<h4>Get in Touch</h4>
+			<p class="paragraf">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum, ipsum ac varius laoreet, turpis nunc tristique lectus, sit amet cursus magna ante a sapien. Donec eu turpis id ligula malesuada faucibus in ut nibh. In mattis consectetur velit a consectetur. Suspendisse egestas ex quis hendrerit pulvinar. Phasellus auctor efficitur aliquam. Donec eget aliquam lectus. Duis dictum condimentum ipsum, ac fringilla justo efficitur sed. </p>
+			<h4 class="office">The Office</h4>
+			<div>
+				<div class="icon_holder">
+					<img class="address icon" src="media/images/icons/address.png">
+					<span class="bold">Address: </span><span class="text_icons">10-123 1/2 Main Street NW Montreal QC Canada</span>
+				</div>
+				<div class="icon_holder">
+					<img class="icon" src="media/images/icons/phone.png">
+					<span class="bold">Phone: </span><span class="text_icons">+1-202-555-0198</span>
+				</div>
+				<div class="icon_holder">
+					<img class="icon" src="media/images/icons/email.png">
+					<span class="bold">Email: </span><span class="text_icons">medhyatrops@gmail.com</span>		
+				</div>
+			<h4 class="social">Social Media</h4>
+			<div class="image_holder">
+				<img src="media/images/icons/fb.png">
+				<img src="media/images/icons/twitter.png">
+				<img src="media/images/icons/linkedin.png">
+			</div>
+			</div>
+		</div>
+	</div>
+	<?php include('footer.html'); ?>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" class="csstransforms csstransforms3d csstransitions">
 <head>
 	<meta charset="utf-8">
 	<meta name="author" content="Irina Dobrohotova">
@@ -9,7 +9,12 @@
 	<link rel="stylesheet" type="text/css" href="styles/index.css">
 	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,600,700|Raleway:300,400,600,700" rel="stylesheet">
 	<title>Medhyatrops</title>
-  <script type="text/javascript" src="scripts/slider.js"></script>
+  <link href="styles/bootstrap.min.css" rel="stylesheet">
+  <link href="styles/font-awesome.min.css" rel="stylesheet">
+  <link href="styles/animate.css" rel="stylesheet">
+  <link href="styles/prettyPhoto.css" rel="stylesheet">
+  <link href="styles/main.css" rel="stylesheet">
+  <link href="styles/responsive.css" rel="stylesheet">
 
 	
 </head>
@@ -17,54 +22,85 @@
 <div class="container">
 	<?php include('header.html'); ?>
 
-  <div class="carousel">
-    <div class="carousel-inner">
-        <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
-        <div class="carousel-item">
-            <img class="mySlides" src="media/images/sliderimage1.png">
-            <div class="carouselDivCloser">
-              <p></p>
-              <p></p>
-              <div><span></span></div>
-            </div>
-        </div>
-        <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
-        <div class="carousel-item">
-            <img class="mySlides" src="media/images/sliderimage2.png">
-            <div class="carouselDivCloser">
-              <p></p>
-              <p></p>
-              <div><span></span></div>
-            </div>
-        </div>
-        <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
-        <div class="carousel-item">
-            <img class="mySlides" src="media/images/sliderimage3.png">
-            <div class="carouselDivFurther">
-              <p></p>
-              <p></p>
-              <div><span></span></div>
-            </div>
-        </div>
-        <label for="carousel-3" class="carousel-control prev control-1">‹</label>
-        <label for="carousel-2" class="carousel-control next control-1">›</label>
-        <label for="carousel-1" class="carousel-control prev control-2">‹</label>
-        <label for="carousel-3" class="carousel-control next control-2">›</label>
-        <label for="carousel-2" class="carousel-control prev control-3">‹</label>
-        <label for="carousel-1" class="carousel-control next control-3">›</label>
-        <ol class="carousel-indicators">
-            <li>
-                <label for="carousel-1" class="carousel-bullet">•</label>
-            </li>
-            <li>
-                <label for="carousel-2" class="carousel-bullet">•</label>
-            </li>
-            <li>
-                <label for="carousel-3" class="carousel-bullet">•</label>
-            </li>
-        </ol>
-    </div>
-  </div>
+    <section id="main-slider" class="no-margin">
+        <div class="carousel slide">
+            <ol class="carousel-indicators">
+                <li data-target="#main-slider" data-slide-to="0" class="active"></li>
+                <li data-target="#main-slider" data-slide-to="1"></li>
+                <li data-target="#main-slider" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+
+                <div class="item active" style="background-image: url(media/images/sliderimage1.png)">
+                    <div class="container">
+                        <div class="row slide-margin">
+                            <div class="col-sm-6">
+                                <div class="carousel-content">
+                                    <h1 class="animation animated-item-1">KLEEMANN product range
+</h1>
+                                    <h2 class="animation animated-item-2">Accusantium doloremque laudantium totam rem aperiam, eaque ipsa...</h2>
+                                    <a class="btn-slide animation animated-item-3" href="#">Read More</a>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 hidden-xs animation animated-item-4">
+                                <div class="slider-img">
+                                    <!-- <img src="images/slider/img1.png" class="img-responsive"> -->
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div><!--/.item-->
+
+                <div class="item" style="background-image: url(media/images/sliderimage2.png)">
+                    <div class="container">
+                        <div class="row slide-margin">
+                            <div class="col-sm-6">
+                                <div class="carousel-content">
+                                    <h1 class="animation animated-item-1">KLEEMANN product range</h1>
+                                    <h2 class="animation animated-item-2">Green Lifts</h2>
+                                    <a class="btn-slide animation animated-item-3" href="#">Read More</a>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 hidden-xs animation animated-item-4">
+                                <div class="slider-img">
+                                    <!-- <img src="images/slider/img2.png" class="img-responsive"> -->
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div><!--/.item-->
+
+                <div class="item" style="background-image: url(media/images/sliderimage3.png)">
+                    <div class="container">
+                        <div class="row slide-margin">
+                            <div class="col-sm-6">
+                                <div class="carousel-content">
+                                    <h1 class="animation animated-item-1">KLEEMANN product range</h1>
+                                    <h2 class="animation animated-item-2">MR Traction and Hydraulic Lifts.</h2>
+                                    <a class="btn-slide animation animated-item-3" href="#">Read More</a>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 hidden-xs animation animated-item-4">
+                                <div class="slider-img">
+                                    <!-- <img src="images/slider/img3.png" class="img-responsive"> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--/.item-->
+            </div><!--/.carousel-inner-->
+        </div><!--/.carousel-->
+        <a class="prev hidden-xs" href="#main-slider" data-slide="prev">
+            <i class="fa fa-chevron-left"></i>
+        </a>
+        <a class="next hidden-xs" href="#main-slider" data-slide="next">
+            <i class="fa fa-chevron-right"></i>
+        </a>
+    </section><!--/#main-slider-->
 
 
 
@@ -85,5 +121,12 @@
 	<!-- <?php include('footer.html'); ?> -->
 
   </div>
+
+  <script src="scripts/jquery.js"></script>
+  <script src="scripts/slider.js"></script>
+  <script src="scripts/bootstrap.min.js"></script>
+  <script src="scripts/jquery.prettyPhoto.js"></script>
+  <script src="scripts/jquery.isotope.min.js"></script>
+  <script src="scripts/wow.min.js"></script>
 </body>
 </html>
