b486ac325a0b5ac01e8fd0fc31df544ea2461ef1
Dobrohotova <irina.dobrohotova@gmail.com>
2017-05-29 23:38:13 +0200

    products page start

---
+++ products.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Irina Dobrohotova">
+	<meta name="description" content="Irina Dobrohotova">
+	<meta name="keywords" content="Medhyatrops">
+	<meta name="viewport" content="">
+	<link rel="stylesheet" type="text/css" href="styles/index.css">
+	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,600,700|Raleway:300,400,600,700" rel="stylesheet">
+	<title>Medhyatrops</title>
+  <link href="styles/products.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+	<?php include('header.html'); ?>
+	<div class="products">
+		<h1>Products</h1>
+		<div class="line1"></div>
+		<div class="line2"></div>
+		<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, ante quis congue ultrices, tortor tellus dictum elit, eu lobortis massa elit nec enim.</h3>
+	</div>
+	<?php include('footer.html'); ?>
+</div>
+</body>
+</html>
