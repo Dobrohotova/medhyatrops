e6e2fa4f71f1102b3c3682f4efe245746dad51d6
Dobrohotova <irina.dobrohotova@gmail.com>
2017-05-30 02:55:03 +0200

    Products Page almost done

--- products.php
+++ products.php
@@ -20,6 +20,67 @@
 		<div class="line2"></div>
 		<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, ante quis congue ultrices, tortor tellus dictum elit, eu lobortis massa elit nec enim.</h3>
 	</div>
+	<div class="left">
+		<div class="dreams">
+			<img class="product" src="media/images/dreams.png">
+		</div>
+		<div class="hp">
+			<img class="product" src="media/images/hp.png">
+		</div>
+		<div class="focus">
+			<img class="product" src="media/images/focus.png">
+		</div>
+	</div>
+	<div class="right">
+		<div class="right_box">
+			<h2 class="dreams_title">Dreams</h2>
+			<p class="price">$10</p>
+			<p class="text"><span class="dreams_span">DREAMS </span>does not contain:</p>
+			<p class="text">Wheat, gluten, dairy, added sugars, artificial flavours, colours, 
+sweeteners or animal products. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, ante quis congue ultrices, tortor tellus dictum elit, eu lobortis massa elit nec enim.</p>
+			<div class="be_contain">
+				<p class="be">be <span class="uppercase">CALM</span></p>
+				<p class="be">be <span class="uppercase">RESTFUL</span></p>
+			</div>
+			<p class="text">Select Quantity</p>
+			<input class="input_dreams" type="number" name="">
+			<div class="dreams_add"><p class="text">Add to cart</p></div>
+		</div>
+		<div class="right_box">
+			<h2 class="hp_title">HP</h2>
+			<p class="price">$10</p>
+			<p class="text"><span class="hp_span">HP </span>does not contain:</p>
+			<p class="text">Wheat, gluten, dairy, added sugars, artificial flavours, colours, 
+sweeteners or animal products. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, ante quis congue ultrices, tortor tellus dictum elit, eu lobortis massa elit nec enim.</p>
+			<div class="be_contain">
+				<p class="boost">BOOST ENERGY</p>
+				<p class="boost">+ METABOLISM</p>
+			</div>
+			<p class="text">Select Quantity</p>
+			<input class="input_hp" type="number" name="">
+			<div class="hp_add"><p class="text">Add to cart</p></div>
+		</div>
+		<div class="right_box">
+			<h2 class="focus_title">Focus</h2>
+			<p class="price">$10</p>
+			<p class="text"><span class="focus_span">FOCUS </span>does not contain:</p>
+			<p class="text">Wheat, gluten, dairy, added sugars, artificial flavours, colours, 
+sweeteners or animal products. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, ante quis congue ultrices, tortor tellus dictum elit, eu lobortis massa elit nec enim.
+</p>
+			<div class="be_contain">
+				<p class="be_focus">be <span class="uppercase">CLEAR</span></p>
+				<p class="be_focus">be <span class="uppercase">ALERT</span></p>
+			</div>
+			<p class="text">Select Quantity</p>
+			<input class="input_focus" type="number" name="">
+			<div class="focus_add"><p class="text">Add to cart</p></div>
+		</div>
+	</div>
+
+
+
+
+
 	<?php include('footer.html'); ?>
 </div>
 </body>
