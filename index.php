9e24925f712f2be0326a0ef554b83c1e3cc78c38
Dobrohotova <irina.dobrohotova@gmail.com>
2017-05-30 23:27:07 +0200

    articles done

--- index.php
+++ index.php
@@ -124,10 +124,71 @@
 				</div>
 			</div>
 		</div>
-		<div class="articles">
-			
+		<div class="articles_container">
+			<div class="articles">
+				<h1>Articles</h1>
+				<div class="line1"></div>
+				<div class="line2"></div>
+				<h3>Read our articles about nutrition products, overall health and advices on ways to improve your workout.</h3>
+			</div>
+			<div class="articles_all">
+				<div class="articles_ind">
+					<img src="media/images/article1.png">
+					<h3 class="article_h">Food health benefits</h3>
+					<div class="article_small">
+						<img src="media/images/icons/article.png">
+						<span>By admin</span>
+						<img src="media/images/icons/article.png">
+						<span>Food</span>
+						<img src="media/images/icons/article.png">
+						<span>18 April 2017</span>						
+					</div>
+					<p class="article_lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut quis eum, dolore laboriosam quidem inventore, repudiandae.</p>
+					<a href="articles.php">
+						<div class="article_button">
+							<p>read more</p>
+						</div>
+					</a>
+				</div>
+				<div class="articles_ind">
+					<img src="media/images/article2.png">
+					<h3 class="article_h">Drink water and liquids</h3>
+					<div class="article_small">
+						<img src="media/images/icons/article.png">
+						<span>By admin</span>
+						<img src="media/images/icons/article.png">
+						<span>Drinks</span>
+						<img src="media/images/icons/article.png">
+						<span>12 December 2016</span>
+					</div>
+					<p class="article_lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, esse accusantium earum molestias. </p>
+					<a href="articles.php">
+						<div class="article_button">
+							<p>read more</p>
+						</div>
+					</a>
+				</div>
+				<div class="articles_ind2">
+					<img src="media/images/article3.png">
+					<h3 class="article_h">Work out to remove stress</h3>
+					<div class="article_small">
+						<img src="media/images/icons/article.png">
+						<span>By admin</span>
+						<img src="media/images/icons/article.png">
+						<span>Sport</span>
+						<img src="media/images/icons/article.png">
+						<span>8 June 2017</span>
+					</div>
+					<p class="article_lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut quis eum, dolore laboriosam quidem inventore, repudiandae.</p>
+					<a href="articles.php">
+						<div class="article_button">
+							<p>read more</p>
+						</div>
+					</a>
+				</div>
+			</div>
 		</div>
-		<div class="testemonials">
+		<div class="testemonials_container">
 			
 		</div>
 		<?php include('footer.html'); ?>
