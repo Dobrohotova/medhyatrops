5675bd0753af1821347ea857cb7a64a52243d8c3
Dobrohotova <irina.dobrohotova@gmail.com>
2017-06-05 13:01:26 +0200

    log in form with css and stuff

--- login.php
+++ login.php
@@ -1,3 +1,41 @@
+<?php
+session_start();
+
+$config = 'mysql:host=127.0.0.1;dbname=login';
+$username = 'root';
+$password = 'root';
+$db = new PDO($config, $username, $password);
+
+
+if (isset($_POST['email']) && $_POST['password']) {
+	$post_mail = trim(strip_tags($_POST['email']));
+	$vemail = filter_var($post_mail, FILTER_VALIDATE_EMAIL);
+	$post_password = trim(strip_tags($_POST['password']));
+
+	if(strlen($post_password) > 0 && $vemail){
+		$sql = 'select * from users where email = :email and password = :password'; 
+		$table_query = $db->prepare($sql);
+		$table_query->bindValue(':email', $vemail);
+		$table_query->bindValue(':password', $post_password);
+		$table_query->execute();
+		$table_res = $table_query->fetchAll(PDO::FETCH_ASSOC);	
+
+		if (count($table_res) > 0) {
+			echo "Successful Log in";
+			$_SESSION['logged in'] = true;
+			$_SESSION['user_email'] = $table_res[0]['email'];
+			$_SESSION['user_id'] = $table_res[0]['id'];
+
+			header('location: admin.php');
+		} else {
+			header('location: login.php');
+		//echo "Unsuccessful Log in";
+		}	
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,6 +59,11 @@
 		<div class="line2"></div>
 		<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, ante quis congue ultrices, tortor tellus dictum elit, eu lobortis massa elit nec enim.</h3>
 	</div>
+	<form action='login.php' method="post" class="form">
+		<input type="email" name="email" class="input">
+		<input type="password" name="password" class="input">
+		<button type="submit" class="button">log in</button>
+	</form>
 </div>
 </body>
 </html>
