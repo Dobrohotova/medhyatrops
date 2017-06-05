fbf20e9a18d6e8c8571376b4bb872cde14b2fb9f
Dobrohotova <irina.dobrohotova@gmail.com>
2017-06-05 15:33:18 +0200

    cart css

--- cart.php
+++ cart.php
@@ -35,6 +35,82 @@ if(!isset($_SESSION['cart'])){
 		<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, ante quis congue ultrices, tortor tellus dictum elit, eu lobortis massa elit nec enim.</h3>
 	</div>
 	<div class="cart_container">
+		<table class="table1">
+			<tr>
+				<th></th>
+				<th></th>
+				<th>Product</th>
+				<th>Price</th>
+				<th>Quantity</th>
+				<th>Total</th>
+			</tr>
+			<tr>
+				<td class="texttable">&times</td>
+				<td><img src="media/images/dreamssmall.png"></td>
+				<td class="texttable">Dreams</td>
+				<td class="texttable">$10</td>
+				<td><form action="cart.php" method="post">
+				<input type="hidden" name="product" value="1">
+
+				<div class="product-cart__number number ispin">
+				     <span class="minus">&ndash;</span>
+				     <input class="count" type="text" value="<?=$_SESSION['cart'][1] == 0 ? 1 :  $_SESSION['cart'][1]?>" name="quantity">
+				     <span class="plus">+</span>
+				</div>
+			</form></td>
+				<td class="texttable">$10</td>
+			</tr>
+			<tr>
+				<td class="texttable">&times</td>
+				<td><img src="media/images/hpsmall.png"></td>
+				<td class="texttable">HP</td>
+				<td class="texttable">$10</td>
+				<td><form action="cart.php" method="post">
+				<input type="hidden" name="product" value="1">
+
+				<div class="product-cart__number number ispin">
+				     <span class="minus">&ndash;</span>
+				     <input class="count" type="text" value="<?=$_SESSION['cart'][1] == 0 ? 1 :  $_SESSION['cart'][1]?>" name="quantity">
+				     <span class="plus">+</span>
+				</div>
+			</form></td>
+				<td class="texttable">$10</td>
+			</tr>
+			<tr>
+				<td class="texttable">&times</td>
+				<td><img src="media/images/focussmall.png"></td>
+				<td class="texttable">Focus</td>
+				<td class="texttable">$10</td>
+				<td><form action="cart.php" method="post">
+				<input type="hidden" name="product" value="1">
+
+				<div class="product-cart__number number ispin">
+				     <span class="minus">&ndash;</span>
+				     <input class="count" type="text" value="<?=$_SESSION['cart'][1] == 0 ? 1 :  $_SESSION['cart'][1]?>" name="quantity">
+				     <span class="plus">+</span>
+				</div>
+			</form></td>
+				<td class="texttable">$10</td>
+			</tr>
+		</table>
+		<div class="table2_container">
+			<p class="table2_h">Cart Totals</p>
+			<table class="table2">
+				<tr>
+					<th>Subtotal</th>
+					<td class="texttable">$10</td>
+				</tr>
+				<tr>
+					<th>Shipping</th>
+					<td class="texttable"></td>
+				</tr>
+				<tr>
+					<th>Total</th>
+					<td class="texttable">$10</td>
+				</tr>
+			</table>
+			<button type="submit" class="button">Proceed to Checkout</button>
+		</div>
 		<?php 
 
 		// print_r($_POST);
@@ -49,5 +125,7 @@ if(!isset($_SESSION['cart'])){
 	</div>
 	<?php include('footer.html') ?>
 </div>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script type="text/javascript" src="scripts/quantity.js"></script>
 </body>
 </html>
